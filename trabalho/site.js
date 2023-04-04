53899b84ac0fd16eebcbf78784edc645a986d3c1
zZanderZz <110944106+zZanderZz@users.noreply.github.com>
2023-04-04 20:40:01 -0300

    Add files via upload

---
+++ trabalho/site.js
@@ -0,0 +1,123 @@
+const btn1998 = document.getElementById('btn1998');
+btn1998.addEventListener('click', () => {
+    window.location.href = '1998.html';
+});
+
+const btn1999 = document.getElementById('btn1999');
+btn1999.addEventListener('click', () => {
+    window.location.href = '1999.html';
+});
+
+const btn2000 = document.getElementById('btn2000');
+btn2000.addEventListener('click', () => {
+    window.location.href = '2000.html';
+});
+
+const btn2001 = document.getElementById('btn2001');
+btn2001.addEventListener('click', () => {
+    window.location.href = '2001.html';
+});
+
+const btn2002 = document.getElementById('btn2002');
+btn2002.addEventListener('click', () => {
+    window.location.href = '2002.html';
+});
+
+const btn2003 = document.getElementById('btn2003');
+btn2003.addEventListener('click', () => {
+    window.location.href = '2003.html';
+});
+
+const btn2004 = document.getElementById('btn2004');
+btn2004.addEventListener('click', () => {
+    window.location.href = '2004.html';
+});
+
+const btn2005 = document.getElementById('btn2005');
+btn2005.addEventListener('click', () => {
+    window.location.href = '2005.html';
+});
+
+const btn2006 = document.getElementById('btn2006');
+btn2006.addEventListener('click', () => {
+    window.location.href = '2006.html';
+});
+
+const btn2007 = document.getElementById('btn2007');
+btn2007.addEventListener('click', () => {
+    window.location.href = '2007.html';
+});
+const btn2008 = document.getElementById('btn2008');
+btn2008.addEventListener('click', () => {
+    window.location.href = '2008.html';
+});
+
+const btn2009 = document.getElementById('btn2009');
+btn2009.addEventListener('click', () => {
+    window.location.href = '2009.html';
+});
+
+const btn2010 = document.getElementById('btn2010');
+btn2010.addEventListener('click', () => {
+    window.location.href = '2010.html';
+});
+
+const btn2011 = document.getElementById('btn2011');
+btn2011.addEventListener('click', () => {
+    window.location.href = '2011.html';
+});
+
+const btn2012 = document.getElementById('btn2012');
+btn2012.addEventListener('click', () => {
+    window.location.href = '2012.html';
+});
+
+const btn2013 = document.getElementById('btn2013');
+btn2013.addEventListener('click', () => {
+    window.location.href = '2013.html';
+});
+
+const btn2014 = document.getElementById('btn2014');
+btn2014.addEventListener('click', () => {
+    window.location.href = '2014.html';
+});
+
+const btn2015 = document.getElementById('btn2015');
+btn2015.addEventListener('click', () => {
+    window.location.href = '2015.html';
+});
+
+const btn2016 = document.getElementById('btn2016');
+btn2016.addEventListener('click', () => {
+    window.location.href = '2016.html';
+});
+
+const btn2017 = document.getElementById('btn2017');
+btn2017.addEventListener('click', () => {
+    window.location.href = '2017.html';
+});
+
+const btn2018 = document.getElementById('btn2018');
+btn2018.addEventListener('click', () => {
+    window.location.href = '2018.html';
+});
+
+const btn2019 = document.getElementById('btn2019');
+btn2019.addEventListener('click', () => {
+    window.location.href = '2019.html';
+});
+
+const btn2020 = document.getElementById('btn2020');
+btn2020.addEventListener('click', () => {
+    window.location.href = '2020.html';
+});
+
+const btn2021 = document.getElementById('btn2021');
+btn2021.addEventListener('click', () => {
+    window.location.href = '2021.html';
+});
+
+const btn2022 = document.getElementById('btn202');
+btn2022.addEventListener('click', () => {
+    window.location.href = '2022.html';
+});
